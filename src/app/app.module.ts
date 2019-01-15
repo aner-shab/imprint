6cbf5ae889ea9db44963b71f8c584199cb4459eb
Aner Shabtai <aner.shab@gmail.com>
2019-01-15 16:57:04 +0200

    Explore page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,18 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { ExplorePage } from '../pages/explore/explore';
 
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
+    ExplorePage,
     HomePage,
     TabsPage
   ],
@@ -27,7 +27,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
+    ExplorePage,
     HomePage,
     TabsPage
   ],
